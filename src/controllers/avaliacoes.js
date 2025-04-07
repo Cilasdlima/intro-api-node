402a27cc3c22f475ef8ef2be5bdf0767d4e8ab57
Cilasdlima <cilas.silva@etec.sp.gov.br>
2025-04-07 20:36:36 -0300

    aula 07/04

--- src/controllers/avaliacoes.js
+++ src/controllers/avaliacoes.js
@@ -16,5 +16,50 @@ module.exports = {
                 dados: error.message
             });
         }
-    }
+    },
+    async cadastrarAvaliacoes(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Cadastrar avaliacoes',    
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    },
+    async editarAvaliacoes(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Editar avaliacoes',    
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    },
+    async apagarAvaliacoes(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Apagar avaliacoes',    
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    },
 }
